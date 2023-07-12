2a590c48232973b1d6649b9ef3dd6bc90af056f0
Younes Bachouche <bachouchemca7@gmail.com>
2023-07-12 19:05:36 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function imgSlider(anything){
+    document.querySelector('.selecto').src = anything;
+
+}
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navigation = document.querySelector('.navigation');
+    toggleMenu.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
